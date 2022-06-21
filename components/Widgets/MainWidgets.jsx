23602937030a898be53b6a2fddf933d505a7f893
ali-h-s21 <aliw.e1@yahoo.com>
2022-06-21 07:37:53 +0300

    changed the right widget's picture and adjusted some styling

--- components/Widgets/MainWidgets.jsx
+++ components/Widgets/MainWidgets.jsx
@@ -4,13 +4,13 @@ export function MainWidgets() {
   return (
     <>
       {/* left widget */}
-      <div className="relative flex flex-col overflow-hidden text-white rounded-xl bg-radialBlack">
-        <h1 className="z-10 flex-1 p-6 text-6xl font-extrabold capitalize bg-red-2000">
+      <div className="relative flex flex-col overflow-hidden text-white rounded-2xl bg-radialBlack">
+        <h1 className="z-10 flex-1 p-8 text-6xl font-extrabold capitalize bg-red-2000">
           Sweet clothes for sweet you
         </h1>
 
         <img
-          className="absolute inline-block float-right max-w-full shrink-1 -right-14 -bottom-16 "
+          className="absolute inline-block float-right max-w-full shrink-1 -right-16 -bottom-20 "
           src="assets/clothes2-1.png"
           alt=""
         />
@@ -27,14 +27,14 @@ export function MainWidgets() {
 
       {/* right widget */}
 
-      <div className="relative flex flex-col overflow-hidden text-white rounded-xl bg-radialBlack">
-        <h1 className="z-10 flex-1 p-6 text-6xl font-extrabold capitalize bg-red-2000">
+      <div className="relative flex flex-col overflow-hidden text-white rounded-2xl bg-radialBlack">
+        <h1 className="z-10 flex-1 p-8 text-6xl font-extrabold capitalize bg-red-2000">
           electronics for everybody
         </h1>
 
         <img
           className="shrink-1 relative bottom-0 -right-24 float-right inline-block max-w-[90%] "
-          src="assets/electronics.png"
+          src="assets/electronics-1.png"
           alt=""
         />
         <div className="absolute z-10 px-6 py-1 overflow-hidden text-white border rounded-md cursor-pointer group right-12 bottom-8 hover:text-black100">
--- components/Widgets/SubWidgets.jsx
+++ components/Widgets/SubWidgets.jsx
@@ -12,30 +12,30 @@ import {
 export function SubWidgets() {
   return (
     <div className="flex flex-col justify-between text-gray50 ">
-      <div className="flex h-32 w-full  cursor-pointer flex-col justify-between rounded-lg bg-radialBlack p-4 text-[26px]">
+      <div className="flex h-32 w-full  cursor-pointer flex-col justify-between rounded-2xl bg-radialBlack p-[18px] text-[26px]">
         <div>
           <BsTagFill className="text-4xl" />
         </div>
         <div>
-          <h3 className=" font-semibold">Today's Deals</h3>
+          <h3 className="font-semibold ">Today's Deals</h3>
         </div>
       </div>
 
-      <div className="flex h-32 w-full  flex-col justify-between rounded-lg bg-radialBlack p-4 text-[26px]">
+      <div className="flex h-32 w-full  flex-col justify-between rounded-2xl bg-radialBlack p-[18px] text-[26px]">
         <div>
           <BsClockFill className="text-4xl" />
         </div>
         <div>
-          <h3 className=" font-semibold">New Arrivals</h3>
+          <h3 className="font-semibold ">New Arrivals</h3>
         </div>
       </div>
 
-      <div className="flex h-32 w-full  flex-col justify-between rounded-lg bg-radialBlack p-4 text-[26px]">
+      <div className="flex h-32 w-full  flex-col justify-between rounded-2xl bg-radialBlack p-[18px] text-[26px]">
         <div>
           <BsStarFill className="text-4xl" />
         </div>
         <div>
-          <h3 className=" font-semibold">Best Sellers</h3>
+          <h3 className="font-semibold ">Best Sellers</h3>
         </div>
       </div>
     </div>
